594506e77a264437df06826e4716e0ca73090687
gowtham <gg4749467@gmail.com>
2026-06-05 20:49:11 +0530

    Day 2 - Added custom port range scanning

--- scanner.py
+++ scanner.py
@@ -1,14 +1,30 @@
 import socket
 
-target = input("Enter IP Address: ")
+target = input("Enter IP Address or Hostname: ")
 
-for port in range(1, 101):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(0.5)
+try:
+    ip = socket.gethostbyname(target)
 
-    result = s.connect_ex((target, port))
+    start_port = int(input("Enter Start Port: "))
+    end_port = int(input("Enter End Port: "))
 
-    if result == 0:
-        print(f"Port {port} OPEN")
+    print(f"\nScanning {ip}...\n")
 
-    s.close()
+    for port in range(start_port, end_port + 1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+
+        result = s.connect_ex((ip, port))
+
+        if result == 0:
+            print(f"Port {port} OPEN")
+
+        s.close()
+
+    print("\nScan Completed.")
+
+except socket.gaierror:
+    print("Invalid hostname or IP address.")
+
+except ValueError:
+    print("Please enter valid port numbers.")
